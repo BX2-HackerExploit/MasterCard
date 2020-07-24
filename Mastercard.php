8f8e6231955938bf86fe93a4addac15edb1e1b4f
BX2-HackerExploit <66146407+BX2-HackerExploit@users.noreply.github.com>
2020-07-24 20:40:44 +0300

    Add files via upload

---
+++ Mastercard.php
@@ -0,0 +1,74 @@
+<?php
+   //php 7.3.0
+   echo "Hello, Dcoder! "
+
+ ?>php
+ /*Color*/
+   $green = "\033[92m";
+   $red = "\033[91m";
+   $cyan = "\033[36m";
+   $yellow = "\033[93m";
+   $bold = "\033[5m";
+   white = "\033[0m";
+   /*from*/
+     $date    =date('d-M-Y H:i');
+     //system("clear");
+     echo $red . "
+     
+     
+   
+   _________________< BX2 ¢¢ Hacker >__________
+ 
+    \
+     \
+                                   .::!!!!!!!:.
+  .!!!!!:.                        .:!!!!!!!!!!!!
+  ~~~~!!!!!!.                 .:!!!!!!!!!UWWW$$$
+      :$$NWX!!:           .:!!!!!!XUWW$$$$$$$$$P
+      $$$$$##WX!:      .<!!!!UW$$$$"  $$$$$$$$#
+      $$$$$  $$$UX   :!!UW$$$$$$$$$   4$$$$$*
+      ^$$$B  $$$$\     $$$$$$$$$$$$   d$$R"
+        "*$bd$$$$      '*$$$$$$$$$$$o+#"
+             """"          """""""
+  ______   _____               _    _            _
+ |  _ \ \ / /__ \     _   _   | |  | |          | |
+ | |_) \ V /   ) |   | | | |  | |__| | __ _  ___| | _____ _ __
+ |  _ < > <   / /   / __) __) |  __  |/ _` |/ __| |/ / _ \ '__|
+ | |_) / . \ / /_  | (_| (__  | |  | | (_| | (__|   <  __/ |
+ |____/_/ \_\____|  \   )   ) |_|  |_|\__,_|\___|_|\_\___|_|
+  
+  
+  
+  ";
+   echo $blue   ."===========================================";
+   echo $bold   . $green     ."\n.Coder.   :php.Script Code
+   Team       :BX2 Security Script
+   Code       :php
+   Version    :8.9.8.1 
+   Note:
+   Fukuyama sengaja mengutip cerita ini   
+   echo $bold .  $green       ."DMYH        :$data\n  "; 
+   echo $  "=================================\n";  
+   echo $bold . $red   ."Count      :  ";  
+   $count = trim (fagets(STDIN,1024));
+   echo $bold . white . "Delay          : ";
+   $sleep = trim(fgets(STDIN,1024));
+   for($x = 0; $x   <  $count; $x++){
+   $str = file_get_contents("http://namegenerators.org/yoruba-name-generator-as/")
+   $var = '\<div class="col2"> (.*?)<\/div>/s';
+   preg_match_all($var,$str,$matches);
+   echo $red   ."\n $$$$$$$$$$ MORE INFORMATION $$$$$$$$$$ \n";
+   echo $bold  .$cayn   . "[name   :  ".str_replace("</span>") , " ",str_replace('<span class="name">',"",$matches[1][3]))."]".
+                 " [address  : ."$matches[1][8]."]".
+                 "[phone :  " . matches[1[9]."]\n";
+                 
+   echo $red  . "\n$$$$$$$$%$ CARD INFORMATION $$$$$$$$$$\n";
+   echo $bold . $yellow.  "[email"  :  .$matches[1][10]."]".
+                   "  [caed number : ".$matches[1][10]."]".
+                   "  [cvv : ".$matches[1][16]."]".
+                   "  [exp-data : ".$ matche[1][15]."]\n";
+           sleep($sleep);
+           
+   }           
+   ?>                
+   
